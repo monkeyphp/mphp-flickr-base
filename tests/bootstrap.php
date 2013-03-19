b607b373e2c8072a1e343b97aa63bee22e925299
David White <git@monkeyphp.com>
2013-03-19 21:26:43 +0000

    Added file comment to bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,10 @@
 <?php
+/**
+ * bootstrap.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrBaseTest
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
 require_once '../vendor/autoload.php';
