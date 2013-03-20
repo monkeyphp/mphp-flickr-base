b80a7bb8a0fde38c9d264717cd35790dbac389a1
David White <git@monkeyphp.com>
2013-03-20 19:58:01 +0000

    Added tests to AbstractResultAdapterTest.php

--- tests/MphpFlickrBaseTest/Adapter/Xml/Result/AbstractResultAdapterTest.php
+++ tests/MphpFlickrBaseTest/Adapter/Xml/Result/AbstractResultAdapterTest.php
@@ -25,127 +25,151 @@ class AbstractResultAdapterTest extends \PHPUnit_Framework_TestCase
     /**
      * Test that we can construct an ResultAdapter with an error Xml string
      * and retrieve the stat value
-     * 
+     *
      * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getStat()
+     * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getStatQuery()
      */
     public function testGetStat()
     {
         $stat = 'fail';
-        $code = '105'; 
+        $code = '105';
         $msg = 'Service currently unavailable';
-        $xml = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="$stat"><err code="$code" msg="$message" /></rsp>';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
         $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
-        
-        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($xml, $parameters), 'AbstractResultAdapter', true);
-        
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
         $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
-        
-        $this->assertTrue(\MphpFlickrBase\Adapter\Interfaces\Result\ResultAdapterInterface::STAT_FAIL, $resultAdapter->isStat());
+
+        $this->assertSame(\MphpFlickrBase\Adapter\Interfaces\Result\ResultAdapterInterface::STAT_FAIL, $resultAdapter->getStat());
     }
-    
+
     /**
      * Test that we can construct a ResultAdapter with an error Xml string
      * and retrieve the isFail boolean value
-     * 
+     *
      * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::isFail()
      */
     public function testIsFail()
     {
         $stat = 'fail';
-        $code = '105'; 
+        $code = '105';
         $msg = 'Service currently unavailable';
-        $xml = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="$stat"><err code="$code" msg="$message" /></rsp>';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
         $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
-        
-        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($xml, $parameters), 'AbstractResultAdapter', true);
-        
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
         $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
-        
+
         $this->assertTrue($resultAdapter->isFail());
     }
-    
+
+    /**
+     * Test that we can construct a ResultAdapter with an error Xml string
+     * and retrieve the isFail boolean value
+     *
+     * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::isOk()
+     */
+    public function testIsOk()
+    {
+        $stat = 'fail';
+        $code = '105';
+        $msg = 'Service currently unavailable';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
+        $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
+        $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
+
+        $this->assertFalse($resultAdapter->isOk());
+    }
+
     /**
      * Test that we can construct an instance of a ResultAdapter with an error
      * xml string and retrieve the errCode value
-     * 
+     *
      * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getErrCode()
+     * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getErrCodeQuery()
      */
     public function testGetErrCode()
     {
         $stat = 'fail';
-        $code = '105'; 
+        $code = '105';
         $msg = 'Service currently unavailable';
-        $xml = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="$stat"><err code="$code" msg="$message" /></rsp>';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
         $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
-        
-        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($xml, $parameters), 'AbstractResultAdapter', true);
-        
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
         $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
-        
+
         $this->assertSame($code, $resultAdapter->getErrCode());
     }
-    
+
     /**
      * Test that we can construct a ResultAdapter instance with an error xml string
      * and retrieve the errMsg value
-     * 
+     *
      * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getErrMsg()
+     * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getErrMsgQuery()
      */
     public function testGetErrMsg()
     {
         $stat = 'fail';
-        $code = '105'; 
+        $code = '105';
         $msg = 'Service currently unavailable';
-        $xml = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="$stat"><err code="$code" msg="$message" /></rsp>';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
         $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
-        
-        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($xml, $parameters), 'AbstractResultAdapter', true);
-        
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
         $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
-        
+
         $this->assertSame($msg, $resultAdapter->getErrMsg());
     }
-    
+
     /**
      * Test that we can retrieve the results value that the ResultAdapter instance
      * was constructed with
-     * 
+     *
      * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getResults()
      */
     public function testGetResults()
     {
         $stat = 'fail';
-        $code = '105'; 
+        $code = '105';
         $msg = 'Service currently unavailable';
-        $xml = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="$stat"><err code="$code" msg="$message" /></rsp>';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
         $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
-        
-        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($xml, $parameters), 'AbstractResultAdapter', true);
-        
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
         $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
-        
-        $this->assertSame($xml, $resultAdapter->getResults());
+
+        $this->assertSame($result, $resultAdapter->getResults());
     }
-    
+
     /**
-     * Test that we can retrieve the parameter value that the ResultAdapter was 
+     * Test that we can retrieve the parameter value that the ResultAdapter was
      * constructed with
-     * 
+     *
      * @covers MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter::getParameters()
      */
     public function testGetParameters()
     {
         $stat = 'fail';
-        $code = '105'; 
+        $code = '105';
         $msg = 'Service currently unavailable';
-        $xml = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="$stat"><err code="$code" msg="$message" /></rsp>';
+        $result = '<?xml version="1.0" encoding="utf-8" ?><rsp stat="' . $stat .'"><err code="' . $code . '" msg="' . $msg . '" /></rsp>';
         $parameters = array('api_key' => '0123456789', 'method' => 'flickr.photos.search');
-        
-        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($xml, $parameters), 'AbstractResultAdapter', true);
-        
+
+        $resultAdapter = $this->getMockForAbstractClass('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', array($result, $parameters), 'AbstractResultAdapter', true);
+
         $this->assertInstanceOf('MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter', $resultAdapter);
-        
+
         $this->assertSame($parameters, $resultAdapter->getParameters());
     }
-    
+
 }
