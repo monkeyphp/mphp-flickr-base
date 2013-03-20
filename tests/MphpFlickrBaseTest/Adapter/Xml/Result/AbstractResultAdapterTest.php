4d84cb9c2254be0f73cf20cbdff92085fb63eb39
David White <git@monkeyphp.com>
2013-03-20 15:16:10 +0000

    Added AbstractResultAdapterTest.php

---
+++ tests/MphpFlickrBaseTest/Adapter/Xml/Result/AbstractResultAdapterTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AbstractResultAdapterTest.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrBaseTest
+ * @package    MphpFlickrBaseTest
+ * @subpackage MphpFlickrBaseTest\Adapter\Xml\Result
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrBaseTest\Adapter\Xml\Result;
+
+/**
+ * AbstractResultAdapterTest
+ *
+ * @category   MphpFlickrBaseTest
+ * @package    MphpFlickrBaseTest
+ * @subpackage MphpFlickrBaseTest\Adapter\Xml\Result
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class AbstractResultAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    //put your code here
+}
