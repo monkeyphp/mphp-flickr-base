88cba023ee4f3ffb0319a05464cfc49f91227349
David White <git@monkeyphp.com>
2013-03-27 21:52:20 +0000

    Added MissingParameterException.php

---
+++ src/MphpFlickrBase/Exception/MissingParameterException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * MissingParameterException.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrBase\Exception;
+
+/**
+ * MissingParameterException
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class MissingParameterException extends \Exception
+{
+
+}
