3566ac69369de23b19172549e96353bbd63cfa89
David White <git@monkeyphp.com>
2013-03-28 21:40:22 +0000

    Added data/resultset.xml and AbstractConnectorTest comments

--- tests/MphpFlickrBaseTest/Connector/AbstractConnectorTest.php
+++ tests/MphpFlickrBaseTest/Connector/AbstractConnectorTest.php
@@ -31,10 +31,29 @@ class AbstractConnectorTest extends \PHPUnit_Framework_TestCase
         $connector = $this->getMockForAbstractClass('MphpFlickrBase\Connector\AbstractConnector', array($apiKey), 'AbstractConnector', true);
     }
 
+    /**
+     * Test that we can construct a Connector instance when we suppy a Http Client instance
+     */
     public function test__constructWithHttpClient()
     {
         $apiKey = '0123456789';
         $httpClient = $this->getMock('Zend\Http\Client');
         $connector = $this->getMockForAbstractClass('MphpFlickrBase\Connector\AbstractConnector', array($apiKey, $httpClient), 'AbstractConnector', true);
+    }
+
+    /**
+     * This isnt working quite as I had hoped
+     */
+    public function testPrepareParameters()
+    {
+        $apiKey = '0123456789';
+        $connector = $this->getMockForAbstractClass('MphpFlickrBase\Connector\AbstractConnector', array($apiKey), 'AbstractConnector', true);
+
+        $reflectionObject = new \ReflectionObject($connector);
+        $reflectionMethod = $reflectionObject->getMethod('prepareParameters');
+        $reflectionMethod->setAccessible(true);
+
+        $parameters = array();
+        $reflectionMethod->invoke($connector, $parameters);
     }
 }
