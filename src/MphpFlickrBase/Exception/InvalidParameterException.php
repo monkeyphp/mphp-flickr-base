0dfa08ba74cd8e09ee1887033cc49b990d278fcc
David White <git@monkeyphp.com>
2013-03-29 12:07:02 +0000

    Added InvalidParameterException.php

---
+++ src/MphpFlickrBase/Exception/InvalidParameterException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * InvalidParameterException.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrBase\Exception;
+
+/**
+ * InvalidParameterException
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class InvalidParameterException extends \Exception
+{
+    
+}
