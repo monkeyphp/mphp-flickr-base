58396e08168f5e50936c5f1a9e578b5b5b861522
David White <git@monkeyphp.com>
2013-04-01 14:41:49 +0100

    Added additional Exception classes

---
+++ src/MphpFlickrBase/Exception/FailResultException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * FailResultException.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrBase\Exception;
+
+/**
+ * FailResultException
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class FailResultException extends \Exception
+{
+}
---
+++ src/MphpFlickrBase/Exception/UnknownResponseFormatException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * UnknownResponseFormatException.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrBase\Exception;
+
+/**
+ * Description of UnknownResponseFormatException
+ *
+ * @category   MphpFlickrBase
+ * @package    MphpFlickrBase
+ * @subpackage MphpFlickrBase\Exception
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class UnknownResponseFormatException extends \Exception
+{
+}
